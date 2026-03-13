c1d641416fe246c6f3a5eba3fa8e28cede906dbc
Kata Rizkyo <135483549+katarizkyo99@users.noreply.github.com>
2026-03-13 13:41:46 +0700

    Add useState import to ChatWindow component

--- frontend/components/ChatWindow.js
+++ frontend/components/ChatWindow.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FiPlus } from "react-icons/fi";
 import ConfirmPopup from "@/components/ConfirmPopup";
 import ReactMarkdown from "react-markdown";
