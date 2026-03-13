42890c19e23bd79debc584dc5ac154c62efd2379
Kata Rizkyo <135483549+katarizkyo99@users.noreply.github.com>
2026-03-13 23:37:34 +0700

    Refactor URL imports and add delete routes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import ChatView, get_chats, get_chat_messages, create_chat, delete_chat,get_riwayat_konfigurasi, add_riwayat_konfigurasi, save_message, execute_config, ping_device, get_network_devices
+from .views import (
+    ChatView, get_chats, get_chat_messages, create_chat, delete_chat,
+    get_riwayat_konfigurasi, add_riwayat_konfigurasi, save_message, 
+    execute_config, ping_device, get_network_devices, delete_riwayat_konfigurasi, 
+    delete_all_riwayat
+)
 urlpatterns = [
     path('chat/', ChatView.as_view(), name='chat'),
     path('chats/', get_chats, name='get_chats'),
@@ -8,6 +13,8 @@ urlpatterns = [
     path("chats/<uuid:chat_id>/delete/", delete_chat, name="delete_chat"),
     path("riwayat-konfigurasi/", get_riwayat_konfigurasi),
     path("riwayat-konfigurasi/add/", add_riwayat_konfigurasi),
+    path("riwayat-konfigurasi/<int:riwayat_id>/delete/", delete_riwayat_konfigurasi),
+    path("riwayat-konfigurasi/delete-all/", delete_all_riwayat),
     path("chats/messages/save/", save_message, name="save_message"),
     path('execute_config/', execute_config),
     path("ping/", ping_device, name="ping_device"),
