6616ade3bac9647c7f6ac497b986f3395ee3d225
Kata Rizkyo <135483549+katarizkyo99@users.noreply.github.com>
2026-04-21 11:47:18 +0700

    Change LLM model from llama-3.1-8b-instant to openai/gpt-oss-120bt

--- backend/api/views.py
+++ backend/api/views.py
@@ -211,7 +211,7 @@ class ChatView(APIView):
               print("▶️ Proses 1 (Text Analyzer) Bekerja...")
               proses_1_reply = call_groq_llm(
                   api_key=api_key, 
-                  model="llama-3.1-8b-instant", 
+                  model="openai/gpt-oss-120bt", 
                   messages=messages_for_llm
               )
 
