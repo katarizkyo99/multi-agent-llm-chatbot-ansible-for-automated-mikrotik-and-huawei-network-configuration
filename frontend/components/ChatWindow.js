3ce942f0cea483046c4be5200ed53db94d77d814
Kata Rizkyo <135483549+katarizkyo99@users.noreply.github.com>
2026-04-27 12:42:01 +0700

    Update ChatWindow.js

--- frontend/components/ChatWindow.js
+++ frontend/components/ChatWindow.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { FiPlus } from "react-icons/fi";
 import ConfirmPopup from "@/components/ConfirmPopup";
-import ReactMarkdown, Markdown from "react-markdown";
+import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
 // Mermaid Graph
