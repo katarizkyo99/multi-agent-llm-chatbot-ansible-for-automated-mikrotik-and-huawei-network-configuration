4769ce3610c267e9ae1977cded5fb234f6a417bb
Kata Rizkyo <135483549+katarizkyo99@users.noreply.github.com>
2026-05-16 11:49:49 +0700

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -254,7 +254,7 @@ class ChatView(APIView):
               
               proses_1_reply = call_groq_llm(
                   api_key=api_key, 
-                  model="llama-3.3-70b-versatile", #openai/gpt-oss-120b #llama-3.3-70b-versatile
+                  model="openai/gpt-oss-120b", #openai/gpt-oss-120b #llama-3.3-70b-versatile
                   messages=messages_for_llm,
                   temperature = 0.1
               )
@@ -300,7 +300,7 @@ class ChatView(APIView):
 
               final_bot_reply = call_groq_llm(
                   api_key=api_key, 
-                  model="openai/gpt-oss-120b", 
+                  model="llama-3.3-70b-versatile", 
                   messages=messages_for_proses_2
               )
 
